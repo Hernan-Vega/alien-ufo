ab4dcbbfae8fdb55422d729a12620a4c95019a8e
Hernan-Vega <ffhernanvega@gmail.com>
2021-05-01 00:18:12 -0300

    Scss library setted.

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,11 @@
+import PropTypes from "prop-types";
 import "./Layout.scss";
 
 function Layout({ children }) {
   return <div className="layout">{children}</div>;
 }
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+};
 
 export { Layout };
