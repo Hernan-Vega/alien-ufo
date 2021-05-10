cfc7b7501c97c91c52f2631d39510cbe17c82057
Hernan-Vega <ffhernanvega@gmail.com>
2021-05-10 00:45:24 -0300

    First try to use API request (didn´t worked).

--- src/components/home/SpaceJam/Tables/Tables.js
+++ src/components/home/SpaceJam/Tables/Tables.js
@@ -1,141 +1,62 @@
+import { useState, useEffect } from "react";
+
+import axios from "axios";
+
 import "./Tables.scss";
 
 function Tables() {
-  const easternTable = [
-    {
-      team: "76ers",
-      city: "Philadelphia",
-      wins: 45,
-      losses: 21,
-      pow: 0.682,
-    },
-    {
-      team: "Bucks",
-      city: "Milkwaukee",
-      wins: 42,
-      losses: 24,
-      pow: 0.636,
-    },
-    {
-      team: "Bulls",
-      city: "Chicago",
-      wins: 26,
-      losses: 39,
-      pow: 0.4,
-    },
-    {
-      team: "Cavaliers",
-      city: "Cleveland",
-      wins: 21,
-      losses: 45,
-      pow: 0.318,
-    },
-    {
-      team: "Celtics",
-      city: "Boston",
-      wins: 35,
-      losses: 31,
-      pow: 0.53,
-    },
-    {
-      team: "Hawks",
-      city: "Atlanta",
-      wins: 37,
-      losses: 30,
-      pow: 0.552,
-    },
-    {
-      team: "Heat",
-      city: "Miami",
-      wins: 35,
-      losses: 31,
-      pow: 0.53,
-    },
-    {
-      team: "Hornets",
-      city: "Charlotte",
-      wins: 32,
-      losses: 33,
-      pow: 0.492,
-    },
-    {
-      team: "Knicks",
-      city: "New York",
-      wins: 37,
-      losses: 29,
-      pow: 0.561,
-    },
-    {
-      team: "Magic",
-      city: "Orlando",
-      wins: 21,
-      losses: 45,
-      pow: 0.318,
-    },
-    {
-      team: "Nets",
-      city: "Brooklyn",
-      wins: 43,
-      losses: 23,
-      pow: 0.652,
-    },
-    {
-      team: "Pacers",
-      city: "Indiana",
-      wins: 30,
-      losses: 35,
-      pow: 0.462,
-    },
-    {
-      team: "Pistons",
-      city: "Detroit",
-      wins: 19,
-      losses: 47,
-      pow: 0.288,
-    },
-    {
-      team: "Raptors",
-      city: "Toronto",
-      wins: 27,
-      losses: 39,
-      pow: 0.409,
-    },
-    {
-      team: "Wizards",
-      city: "Washinton",
-      wins: 30,
-      losses: 36,
-      pow: 0.455,
-    },
-  ];
+  const [premier, setPremier] = useState([]);
 
-  const orderedArr = [...easternTable].sort((a, b) =>
-    a.pow === b.pow ? 0 : a.pow < b.pow ? 1 : -1
-  );
+  useEffect(() => {
+    const options = {
+      method: "GET",
+      url:
+        "https://api.football-data.org/v2/competitions/2021/standings?standingType=HOME",
+      headers: {
+        "User-Agent": "PostmanRuntime/7.28.0",
+        Accept: "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        "X-Auth-Token": "2d3fb3b10ae64c25875b5c342f75d68b",
+      },
+    };
+
+    async function getTable() {
+      const myTable = await axios.request(options);
+      try {
+        setPremier(myTable.standings[0].table);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getTable();
+  }, []);
 
   return (
     <div claassName="left">
       <div className="left__buttons">
         <button type="button" className="left__buttons__east">
-          East
+          Premier League
         </button>
       </div>
       <div className="left__table">
         <table>
           <tr>
+            <th>Position</th>
             <th>Team</th>
+            <th>Points</th>
             <th>W</th>
+            <th>D</th>
             <th>L</th>
-            <th>% winning</th>
           </tr>
-          {orderedArr.map(({ team, city, wins, losses, pow }) => (
-            <tr key={team}>
-              <td>
-                {city} {team}
-              </td>
-              <td>{wins}</td>
-              <td>{losses}</td>
-              <td>{pow}</td>
+          {premier.map(({ position, team, points, won, draw, lost }) => (
+            <tr key={team.name}>
+              <td>{position}</td>
+              <td>{team.name}</td>
+              <td>{points}</td>
+              <td>{won}</td>
+              <td>{draw}</td>
+              <td>{lost}</td>
             </tr>
           ))}
         </table>
